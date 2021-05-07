1fb22a6f6229490c08361712b33e32c70150c068
Levente Wolf <wlevi2002@gmail.com>
2021-05-07 14:10:19 +0200

    Extract and generalize imageRotation method

---
+++ src/main/java/hu/wolf/RotationHandler.java
@@ -0,0 +1,24 @@
+package hu.wolf;
+
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class RotationHandler {
+
+    /**
+     * Rotate the image and imageView by a given degree <br>
+     *     The rotated image is a snapshot of the imageView
+     * @param imageView which contains the image
+     * @param rotationDegree the degree at which we rotate the ImageView
+     * @return a snapshot Image from the rotated ImageView
+     */
+    public static Image rotateImage(ImageView imageView, int rotationDegree) {
+        imageView.setRotate(imageView.getRotate() + rotationDegree);
+        SnapshotParameters params = new SnapshotParameters();
+
+        Image rotatedImage = imageView.snapshot(params, null);
+
+        return rotatedImage;
+    }
+}
