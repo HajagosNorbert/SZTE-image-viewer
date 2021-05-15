0ffe530f8b55bcab1fd0c9dd75f8653380ff78bc
Levente Wolf <wlevi2002@gmail.com>
2021-05-15 15:22:09 +0200

    Extract isEveryPixelTheSame method to a new class

--- src/main/Test/hu/wolf/InversionHandlerTest.java
+++ src/main/Test/hu/wolf/InversionHandlerTest.java
@@ -15,8 +15,6 @@ import java.io.IOException;
 import static org.junit.Assert.assertTrue;
 
 public class InversionHandlerTest {
-    //Saving worsen the original image resolution
-    private final static double ERROR_LIMIT = 0.1;
 
     @Test
     public void testImageInversion() throws IOException {
@@ -30,56 +28,7 @@ public class InversionHandlerTest {
         BufferedImage picture2 = ImageIO.read(file2);
         Image result = InversionHandler.invertImage(SwingFXUtils.toFXImage(picture2, null));
 
-        assertTrue(isEveryPixelTheSame(expected, result));
+        assertTrue(ImageManipulator.isEveryPixelTheSame(expected, result));
     }
 
-    /**
-     * This method checks that if all the pixels
-     * in the two images are the same within the error limit
-     * @param expectedImage the expected inverted image
-     * @param resultImage the inverted image we got from our invertImage() method
-     * @return true if every pixel is the same else false
-     */
-    private boolean isEveryPixelTheSame(Image expectedImage, Image resultImage) {
-        if (expectedImage == null || resultImage == null) {
-            return false;
-        }
-
-        int imageWidth = (int) expectedImage.getWidth();
-        int imageHeight = (int) expectedImage.getHeight();
-
-        PixelReader resultReader = resultImage.getPixelReader();
-        PixelReader expectedReader = expectedImage.getPixelReader();
-
-        Color expectedColor;
-        Color resultColor;
-
-        for (int y = 0; y < imageHeight; y++) {
-            for (int x = 0; x < imageWidth; x++) {
-                expectedColor = expectedReader.getColor(x, y);
-                resultColor = resultReader.getColor(x, y);
-
-                double expectedR = expectedColor.getRed();
-                double expectedG = expectedColor.getGreen();
-                double expectedB = expectedColor.getBlue();
-
-                double resultR = resultColor.getRed();
-                double resultG = resultColor.getGreen();
-                double resultB = resultColor.getBlue();
-
-                if (Math.abs(expectedR - resultR) > ERROR_LIMIT || Math.abs(expectedG - resultG) > ERROR_LIMIT || Math.abs(expectedB - resultB) > ERROR_LIMIT) {
-                    System.err.printf("At pixel x: %d y: %d\n The two colors dont match\n", x, y);
-
-                    System.err.println("expected:");
-                    System.err.printf("R: %.0f G: %.0f B: %.0f\n", expectedR * 255, expectedB * 255, expectedG * 255);
-
-                    System.err.println("resulted:");
-                    System.err.printf("R: %.0f G: %.0f B: %.0f\n", resultR * 255, resultB * 255, resultG *255);
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
 }
